b01a5c738eb5897c74996d34d08d05ac8edb5331
Davide Gallitelli <dggallit@amazon.com>
2021-12-21 12:24:19 +0100

    Updates to init and template

--- init.sh
+++ init.sh
@@ -12,17 +12,12 @@ then
 fi
 
 mkdir -p outputs
-cd 'seedcode/async-inference/'; zip -qr9 ../../outputs/async-seedcode.zip .
-cd '../../seedcode/batch-inference/'; zip -qr9 ../../outputs/batch-seedcode.zip .
-cd '../../seedcode/realtime-inference/'; zip -qr9 ../../outputs/realtime-seedcode.zip .
+cd 'seedcode/async-inference/'; zip -qr9 ../../outputs/async-seedcode.zip . ; cd ../../
+cd 'seedcode/batch-inference/'; zip -qr9 ../../outputs/batch-seedcode.zip . ; cd ../../
+cd 'seedcode/realtime-inference/'; zip -qr9 ../../outputs/realtime-seedcode.zip . ; cd ../../
 
-cd ../..
 for filename in outputs/*.zip; do
-    aws s3 cp $filename s3://$AWS_BUCKET/$SEEDCODE_PATH/$filename --quiet
+    aws s3 cp $filename s3://$AWS_BUCKET/$SEEDCODE_PATH/$filename
 done
+
 echo "Upload of seedcodes successful."
-
-
-sed -i "" "s|<#####BUCKET-PLACEHOLDER#####>|$AWS_BUCKET|" "./template.yml"
-sed -i "" "s|<#####FILE-PATH-PLACEHOLDER#####>|$SEEDCODE_PATH|" "./template.yml"
-echo "Update of the template file complete."
